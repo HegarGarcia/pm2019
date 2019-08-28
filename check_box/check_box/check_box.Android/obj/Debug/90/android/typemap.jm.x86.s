62020de9438f06c1c4093a1627ea25aa84f56936
Hegar Garcia <hegargarcia@gmail.com>
2019-08-28 16:02:12 +0200

    Adding check box and radio button

---
+++ check_box/check_box/check_box.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 490f27773df46ea94690e5e8de6c9002367b9c6c */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ radio_button/radio_button/radio_button.Android/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 41870d2eb92e85f3c845392b6ea02709ebc0701a */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1349
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 353439
+	.include	"typemap.jm.inc"
