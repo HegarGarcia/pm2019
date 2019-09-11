d2c30c9194b478cc6adf6d7eb093dc99b8bffabf
Hegar Garcia <hegargarcia@gmail.com>
2019-09-10 22:59:32 -0500

    Adding sells list page

---
+++ pos/190918_pos/190918_pos/SellsList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace 190918_pos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class SellsList : ContentPage
+{
+    public SellsList()
+    {
+        InitializeComponent();
+    }
+}
+}
