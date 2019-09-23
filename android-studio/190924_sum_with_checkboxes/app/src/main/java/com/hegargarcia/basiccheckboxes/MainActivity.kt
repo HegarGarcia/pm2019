2ae791454be1156db475fb055508b01d6427925e
Hegar Garcia <hegargarcia@gmail.com>
2019-09-23 09:34:31 -0500

    Adding business logic for check boxes and spinner app

--- android-studio/190924_sum_with_checkboxes/app/src/main/java/com/hegargarcia/basiccheckboxes/MainActivity.kt
+++ android-studio/190924_sum_with_checkboxes/app/src/main/java/com/hegargarcia/basiccheckboxes/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.hegargarcia.basiccheckboxes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        actionButton.setOnClickListener {
+            val firstValue = firstEntry.text.toString().toInt()
+            val secondValue = secondEntry.text.toString().toInt()
+            val result = if (addCheckbox.isChecked) getString(
+                R.string.result_add,
+                firstValue,
+                secondValue,
+                firstValue + secondValue
+            ) else if (subtractCheckbox.isChecked) getString(
+                R.string.result_add,
+                firstValue,
+                secondValue,
+                firstValue - secondValue
+            ) else ""
+
+            resultText.text = result
+        }
     }
 }
--- android-studio/190924_sum_with_spinner/app/src/main/java/com/hegargarcia/basicspinner/MainActivity.kt
+++ android-studio/190924_sum_with_spinner/app/src/main/java/com/hegargarcia/basicspinner/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.hegargarcia.basicspinner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemList = resources.getStringArray(R.array.actions)
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item,
+            itemList
+        )
+        actionSelector.adapter = adapter
+
+        actionButton.setOnClickListener {
+            val firstValue = firstEntry.text.toString().toInt()
+            val secondValue = secondEntry.text.toString().toInt()
+            val result = when(actionSelector.selectedItem.toString()) {
+                itemList[0] -> getString(R.string.result_add, firstValue, secondValue, firstValue + secondValue)
+                itemList[1] -> getString(R.string.result_subtract, firstValue, secondValue, firstValue - secondValue)
+                else -> ""
+            }
+
+            resultText.text = result
+        }
     }
 }
