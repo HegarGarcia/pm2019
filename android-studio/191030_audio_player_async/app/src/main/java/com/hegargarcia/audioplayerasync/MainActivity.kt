2b586b3fd1c63a41e31b0b88bf5596f123c29928
Hegar Garcia <hegargarcia@gmail.com>
2019-10-28 12:21:12 -0600

    Adding Audio Player Async project

---
+++ android-studio/191030_audio_player_async/app/src/main/java/com/hegargarcia/audioplayerasync/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.hegargarcia.audioplayerasync
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer()
+
+        mediaPlayer.setOnPreparedListener {
+            it.start()
+        }
+
+        playButton.setOnClickListener {
+            mediaPlayer.reset()
+            try {
+                mediaPlayer.apply {
+                    setDataSource("https://firebasestorage.googleapis.com/v0/b/bebin-baacpa.appspot.com/o/iphone-notificacion.mp3?alt=media&token=cf24a16d-4a9b-4a1c-a370-10040f08a181")
+                    prepareAsync()
+                }
+            } catch (e: IOException) {
+                Toast.makeText(this, R.string.error, Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
